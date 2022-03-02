7aa2d3411ed41bdeff59b0dd542b541efa710752
Bastien Brisson <bastien.brisson@etudiant.univ-rennes1.fr>
2022-03-02 10:53:41 +0100

    1ere scéance : question 1 à 6

--- interaction.js
+++ interaction.js
@@ -3,10 +3,33 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
-
+    this.initX = 0;
+    this.initY = 0;
+    this.finalX = 0;
+    this.finalY = 0;
+    this.isClicked = false;
 	// Developper les 3 fonctions gérant les événements
+    this.clic = function (evt) {
+            this.isClicked = true;
+    }.bind(this);
+
+    this.deplace = function (evt) {
+        if (this.isClicked) {
+            var position = getMousePosition(canvas,evt);
+            this.finalX = position.x;
+            this.finalY = position.y;
+            console.log(evt);
+        }
+    }.bind(this);
+
+    this.deClic = function (evt) {
+        this.isClicked = false;
+    }.bind(this);
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mousedown', this.clic, false);
+    canvas.addEventListener('mousemove', this.deplace, false);
+    canvas.addEventListener('mouseup', this.deClic, false);
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,33 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing() {
+
+	this.array = new Array();
+
+
+};
+
+function Forme (color, width) {
+    this.color = color;
+    this.initX = initX;
+};
+
+function Rectangle (initX, initY, largeur, hauteur, color, width) {
+    Forme.call(color, width);
+    this.initX = initX;
+    this.initY = initY;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+};
+Rectangle.prototype = new Forme();
+
+
+function Ligne (initX, initY, finalX, finalY, color, width) {
+    Forme.call(color, width);
+    this.initX = initX;
+    this.initY = initY;
+    this.finalX = finalX;
+    this.finalY = finalY;
+};
+Ligne.prototype = new Forme();
